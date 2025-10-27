16d5a9dbaa5f0f8ec8037201aa7999883c284dfe
Duman Haydar <haydar@duman.be>
2025-10-27 13:45:55 +0100

    chore: add test report

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    reporters: ["default", "junit"],
+
+    outputFile: {
+      junit: "junit-report.xml",
+    },
+  },
+});
